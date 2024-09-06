d13ab9737bcd70fd8c53cdc1d3c2c1d0a2a76cd9
Nikoli <huseynlinihad4400@gmail.com>
2024-09-06 12:25:03 +0400

    Layers Are Created.

---
+++ AccountingServer.Presentation/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using AccountingServer.Presentation.Abstraction;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountingServer.Presentation.Controllers
+{
+	public sealed class TestController : ApiController
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok("İşlem Başarılı.");
+		}
+	}
+}
